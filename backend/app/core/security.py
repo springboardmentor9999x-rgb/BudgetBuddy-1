cb1da6119bd5bd8b12e595acbfddcfbd9a5697c3
Abhi-kr-Singh <abhikrsinghdimapur2003@gmail.com>
2026-08-04 20:00:24 +0530

    core & security

---
+++ backend/app/core/security.py
@@ -0,0 +1,61 @@
+from datetime import datetime, timedelta, timezone
+from passlib.context import CryptContext
+from jose import jwt
+
+from app.configs.settings import Settings
+
+pwd_context = CryptContext(schemes=["argon2"], deprecated="auto")
+
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
+
+def verify_password(plain: str, hashed: str) -> bool:
+    return pwd_context.verify(plain, hashed)
+
+def create_access_token(data: dict, expires_delta: timedelta = None)-> str:
+    """_summary_
+
+    Args:
+        data (dict): data to encode in the JWT token (e.g., id, email, role)
+        expires_delta (timedelta, optional): The time duration for which the token will be valid. Defaults to None.
+
+    Returns:
+        _str_: The encoded JWT token.
+    """
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(minutes=Settings.ACCESS_TOKEN_EXPIRE_MINUTES))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, Settings.SECRET_KEY, algorithm=Settings.ALGORITHM)
+
+def create_refresh_token(data: dict, expires_delta: timedelta = None) -> str:
+    """_summary_
+
+    Args:
+        data (dict): data to encode in the JWT token (e.g., id, email, role)
+        expires_delta (timedelta, optional): The time duration for which the token will be valid. Defaults to None.
+
+    Returns:
+        _str_: The encoded JWT token.
+    """
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + (expires_delta or timedelta(days=Settings.REFRESH_TOKEN_EXPIRE_DAYS))
+    to_encode.update({"exp": expire})
+    return jwt.encode(to_encode, Settings.REFRESH_TOKEN_SECRET_KEY, algorithm=Settings.ALGORITHM)
+
+def verify_refresh_token(token: str) -> dict:
+    """_summary_
+
+    Args:
+        token (str): The JWT token to verify.
+
+    Returns:
+        dict: The decoded data from the JWT token.
+
+    Raises:
+        jwt.JWTError: If the token is invalid or expired.
+    """
+    try:
+        payload = jwt.decode(token, Settings.REFRESH_TOKEN_SECRET_KEY, algorithms=[Settings.ALGORITHM])
+        return payload
+    except jwt.JWTError as e:
+        raise e
